de1f28a56d9cf4c8239ffaf35fa81a0365a1496e
Bahal Ahmad <114356169+bahalahmad@users.noreply.github.com>
2023-08-19 16:26:10 +0530

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -1,1 +1,15 @@
-
+export const footer = () => {
+    return (
+        `<div id="footer">
+            <p class="text-center">Made with ❤️ by Bahal Ahmad</p>
+            <div>
+                <button class="btn btn-primary">
+                    <a href="https://www.linkedin.com/in/bahal-ahmad-4723601a7/" target="_blank">LinkedIn</a>
+                </button>
+                <button class="btn btn-success">
+                    <a href="https://github.com/bahalahmad" target="_blank">GitHub</a>
+                </button>
+             </div>
+        </div>`
+    );
+};
